6893629b66881ea62a2ccde2f40c6558697b996f
Kent Stanton <kent.stanton@gmail.com>
2015-06-17 07:32:59 -0400

    Base test harness and first test for system dev tdd
    created the most basic test runner and a script to demo devops tdd

---
+++ tests/test-foldersexist.ps1
@@ -0,0 +1,19 @@
+﻿# test-foldersexist
+
+
+$projectRoot = "c:\myworld\cgis"
+
+$folderList = @("projects","scripts","data")
+$testpasses = $true
+foreach ($folder in $folderList) {
+    $fullPath = "$projectRoot\$folder"
+    $testpasses = Test-Path $fullPath
+}
+
+if ( $testpasses -eq $false) {
+    displayTestResult "FAIL: test-foldersexist - path: $fullpath" $false
+} else {
+    displayTestResult "FAIL: test-foldersexist - path: $fullpath" $true
+}
+
+
---
+++ tests/test-runall_dotsource.ps1
@@ -0,0 +1,15 @@
+﻿
+
+cls
+
+Function displayTestResult ($testResult, $passed) {
+    if ($passed -eq $true) {
+        write-host $testResult -backgroundcolor green -foregroundcolor white
+    } else {
+        write-host $testResult -backgroundcolor red -foregroundcolor white
+    }
+ }  
+
+
+. "$PSScriptRoot\test-foldersexist.ps1"
+
